24251a260c06c78be0838dedfc113f76ef1addfd
Nirav Joshi <33543795+NiravJoshi33@users.noreply.github.com>
2023-08-09 22:30:56 +0530

    User can now add new formats

--- files_organizer.py
+++ files_organizer.py
@@ -1,9 +1,8 @@
 import os
 import shutil
-import datetime
 
 extension_data = {
-    "compressed": ["zip", "7zip", "rar", "gz"],
+    "compressed": ["zip", "7zip", "gz"],
     "audio": ["mp3", "aac", "flac", "wav", "aiff", "dsd", "pcm"],
     "video": ["mp4", "mov", "wmv", "avi", "avchd", "flv", "f4v", "swf", "mkv", "webm", "html5", "mpeg-2", "srt"],
     "picture": ["jpg", "png", "jpeg"],
@@ -11,12 +10,31 @@ extension_data = {
     "documents": ["pdf", "docx", "xlsx", "txt"]
 }
 
+import datetime
+
+def add_format():
+   type1 = input("Please enter category name: (compressed/audio/video/picture/programs/documents)")
+   new_ext = input("Please enter the new extension:")
+   extension_data[type1].append(new_ext)
+   print(f"Updated extension list:\n {extension_data}")
+
+print("This software can handle following formats: \n")
+print(extension_data)
+
+ans = input("Please type 'yes' if you want to add new format").lower()
+if ans == "yes":
+   add_format()
+   
+
 file = open(r"C:\Users\nirav\Downloads\documents\file_organizer_log.txt", "a")
 file.write(f"{datetime.datetime.now()} - The script ran\n")
+
+
 # path = input("Please enter the path:")
 path = r"C:\Users\nirav\Downloads"
 files = os.listdir(path)
 
+print("Found following files")
 for file in files:
     filename, extension = os.path.splitext(file)
     print(filename)
